150a7b6f6c6c58ed0a010121c1cb66dfb6ff447d
Ramon Smits <ramon.smits@gmail.com>
2024-07-12 12:01:31 +0200

    ✨ Log output defaults to systemd, also use NServiceBus.Extensions.Logging to ensure any log output from transports it logged

--- src/ServiceControl.Connector.MassTransit.Host/Program.cs
+++ src/ServiceControl.Connector.MassTransit.Host/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus.Extensions.Logging;
 
 Console.OutputEncoding = Encoding.UTF8;
 Console.WriteLine("\x1b[1;37;5mTServiceControl.Connector.MassTransit.Host\x1b[0m");
@@ -7,6 +10,24 @@ Console.WriteLine("\x1b[1;37;5mTServiceControl.Connector.MassTransit.Host\x1b[0m
 var builder = Host.CreateApplicationBuilder(args);
 builder.UseMassTransitConnector();
 
+var isConsole = args.Contains("-c") || args.Contains("--console");
+
+if (isConsole)
+{
+    builder.Logging.AddSimpleConsole(o =>
+    {
+        o.SingleLine = true;
+        o.IncludeScopes = true;
+    });
+}
+else
+{
+    builder.Logging.AddSystemdConsole();
+}
+
 using var host = builder.Build();
+
+var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
+NServiceBus.Logging.LogManager.UseFactory(new ExtensionsLoggerFactory(loggerFactory));
 
 await host.RunAsync();
