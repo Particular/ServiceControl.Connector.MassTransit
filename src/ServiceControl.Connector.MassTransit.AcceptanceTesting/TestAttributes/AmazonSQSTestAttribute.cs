62eb3d17d7ecac62d80203fae1888dba19fcd5ad
SzymonPobiega <szymon.pobiega@gmail.com>
2024-08-26 12:46:32 +0200

    Change ignore condition

--- src/ServiceControl.Connector.MassTransit.AcceptanceTesting/TestAttributes/AmazonSQSTestAttribute.cs
+++ src/ServiceControl.Connector.MassTransit.AcceptanceTesting/TestAttributes/AmazonSQSTestAttribute.cs
@@ -9,7 +9,7 @@ public class AmazonSQSTestAttribute : Attribute, IApplyToContext
     public void ApplyToContext(TestExecutionContext context)
     {
         var connectionString = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
-        if (string.IsNullOrWhiteSpace(connectionString))
+        if (connectionString == null)
         {
             Assert.Ignore("Ignoring because environment variable AWS_ACCESS_KEY_ID is not available");
         }
