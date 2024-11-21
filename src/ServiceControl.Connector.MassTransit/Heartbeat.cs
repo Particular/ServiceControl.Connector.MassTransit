d194cbd4e89ea350ec962c875a10cd74c1aef884
Phil Bastian <phil.bastian@particular.net>
2024-11-21 15:54:03 +0800

    fix namespace and using declarations

--- src/ServiceControl.Connector.MassTransit/Heartbeat.cs
+++ src/ServiceControl.Connector.MassTransit/Heartbeat.cs
@@ -1,78 +1,77 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿namespace ServiceControl.Connector.MassTransit;
+
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NServiceBus.Routing;
 using NServiceBus.Transport;
 using ServiceControl.Monitoring;
 
-namespace ServiceControl.Connector.MassTransit
+public class Heartbeat(
+    ILogger<Service> logger,
+    TransportDefinitionFactory transportDefinitionFactory,
+    IQueueInformationProvider queueInformationProvider,
+    IQueueFilter queueFilter,
+    Configuration configuration
+    ) : BackgroundService
 {
-    public class Heartbeat(
-        ILogger<Service> logger,
-        TransportDefinitionFactory transportDefinitionFactory,
-        IQueueInformationProvider queueInformationProvider,
-        IQueueFilter queueFilter,
-        Configuration configuration
-        ) : BackgroundService
-    {
 
-        internal const string ConnectedApplicationName = "MassTransitConnector";
+    internal const string ConnectedApplicationName = "MassTransitConnector";
 
 #pragma warning disable PS0018
-        async Task<string[]> GetReceiveQueues()
+    async Task<string[]> GetReceiveQueues()
 #pragma warning restore PS0018
+    {
+        try
         {
-            try
-            {
-                var queues = await queueInformationProvider.GetQueues();
-                return queues.Where(queueFilter.IsMatch).ToArray();
-            }
-            catch (Exception e)
-            {
-                logger.LogWarning(e, "Failure querying the queue information");
-            }
-
-            return [];
+            var queues = await queueInformationProvider.GetQueues();
+            return queues.Where(queueFilter.IsMatch).ToArray();
         }
+        catch (Exception e)
+        {
+            logger.LogWarning(e, "Failure querying the queue information");
+        }
+
+        return [];
+    }
 
 #pragma warning disable PS0017
-        protected override async Task ExecuteAsync(CancellationToken shutdownToken = default)
+    protected override async Task ExecuteAsync(CancellationToken shutdownToken = default)
 #pragma warning restore PS0017
+    {
+        try
         {
-            try
+            var endpointConfiguration = new EndpointConfiguration(configuration.ReturnQueue);
+            var serializer = endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+            endpointConfiguration.Recoverability().Delayed(settings => settings.NumberOfRetries(0));
+            endpointConfiguration.SendOnly();
+
+            var routing = endpointConfiguration.UseTransport(transportDefinitionFactory.CreateTransportDefinition());
+            routing.RouteToEndpoint(typeof(ConnectedApplication), configuration.ControlQueue);
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+            while (!shutdownToken.IsCancellationRequested)
             {
-                var endpointConfiguration = new EndpointConfiguration(configuration.ReturnQueue);
-                var serializer = endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-                endpointConfiguration.Recoverability().Delayed(settings => settings.NumberOfRetries(0));
-                endpointConfiguration.SendOnly();
+                var massTransitErrorQueues = await GetReceiveQueues();
 
-                var routing = endpointConfiguration.UseTransport(transportDefinitionFactory.CreateTransportDefinition());
-                routing.RouteToEndpoint(typeof(ConnectedApplication), configuration.ControlQueue);
-
-                var endpointInstance = await Endpoint.Start(endpointConfiguration);
-
-                while (!shutdownToken.IsCancellationRequested)
+                using var scope = logger.BeginScope("HEARTBEAT");
+                await endpointInstance.Send(new ConnectedApplication
                 {
-                    var massTransitErrorQueues = await GetReceiveQueues();
+                    Application = ConnectedApplicationName,
+                    SupportsHeartbeats = false,
+                    ErrorQueues = massTransitErrorQueues,
+                });
 
-                    using var scope = logger.BeginScope("HEARTBEAT");
-                    await endpointInstance.Send(new ConnectedApplication
-                    {
-                        Application = ConnectedApplicationName,
-                        SupportsHeartbeats = false,
-                        ErrorQueues = massTransitErrorQueues,
-                    });
-
-                    await Task.Delay(configuration.HeartbeatInterval, shutdownToken);
-                }
-
-                await endpointInstance.Stop();
-            }
-            catch (OperationCanceledException) when (shutdownToken.IsCancellationRequested)
-            {
-                // Ignore
+                await Task.Delay(configuration.HeartbeatInterval, shutdownToken);
             }
 
-            await StopAsync(shutdownToken);
+            await endpointInstance.Stop();
         }
+        catch (OperationCanceledException) when (shutdownToken.IsCancellationRequested)
+        {
+            // Ignore
+        }
+
+        await StopAsync(shutdownToken);
     }
 }
