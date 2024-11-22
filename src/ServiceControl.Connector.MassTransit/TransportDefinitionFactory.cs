2effc52ed071fec4ec1726def65c691141bbc5ec
Ramon Smits <ramon.smits@gmail.com>
2024-11-22 11:02:53 +0100

    Resolve PS0003

--- src/ServiceControl.Connector.MassTransit/TransportDefinitionFactory.cs
+++ src/ServiceControl.Connector.MassTransit/TransportDefinitionFactory.cs
@@ -1,16 +1,26 @@
 using NServiceBus.Transport;
 
-public class TransportDefinitionFactory
+public sealed class TransportDefinitionFactory
 {
     readonly Func<HostSettings, ReceiveSettings[], string[], CancellationToken, Task<TransportInfrastructure>> factoryMethod;
 
-    public TransportDefinitionFactory(Func<HostSettings, ReceiveSettings[], string[], CancellationToken, Task<TransportInfrastructure>> factoryMethod)
+    public TransportDefinitionFactory(
+        Func<HostSettings,
+        ReceiveSettings[],
+        string[],
+        CancellationToken,
+        Task<TransportInfrastructure>> factoryMethod
+    )
     {
         this.factoryMethod = factoryMethod;
     }
 
-    public async Task<TransportInfrastructure> CreateTransportInfrastructure(HostSettings hostSettings, ReceiveSettings[] receivers,
-        string[] sendingAddresses, CancellationToken cancellationToken)
+    public async Task<TransportInfrastructure> CreateTransportInfrastructure(
+        HostSettings hostSettings,
+        ReceiveSettings[] receivers,
+        string[] sendingAddresses,
+        CancellationToken cancellationToken = default
+    )
     {
         return await factoryMethod(hostSettings, receivers, sendingAddresses, cancellationToken);
     }
