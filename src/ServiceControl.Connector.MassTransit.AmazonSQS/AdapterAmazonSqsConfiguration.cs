0a99f1662840a18c747858cdf66c20032d394504
Ramon Smits <ramon.smits@gmail.com>
2024-11-26 14:26:26 +0100

    Updated to NServiceBus.AmazonSQS 7.1.0 that has new constructor that allows disabling delayed delivery

--- src/ServiceControl.Connector.MassTransit.AmazonSQS/AdapterAmazonSqsConfiguration.cs
+++ src/ServiceControl.Connector.MassTransit.AmazonSQS/AdapterAmazonSqsConfiguration.cs
@@ -1,3 +1,4 @@
+using Amazon.SimpleNotificationService;
 using Amazon.SQS;
 using Microsoft.Extensions.DependencyInjection;
 using NServiceBus.Transport;
@@ -8,14 +9,18 @@ public static class AdapterAmazonSqsConfiguration
     {
         services.AddSingleton<IQueueInformationProvider>(new AmazonSqsHelper(string.Empty));
         services.AddSingleton<IAmazonSQS, AmazonSQSClient>();
-        services.AddSingleton(sp => new TransportDefinitionFactory(async (HostSettings hostSettings, ReceiveSettings[] receivers,
-            string[] sendingAddresses, CancellationToken cancellationToken) =>
+        services.AddSingleton<IAmazonSimpleNotificationService, AmazonSimpleNotificationServiceClient>();
+        services.AddSingleton(sp => new TransportDefinitionFactory(async (HostSettings hostSettings, ReceiveSettings[] receivers, string[] sendingAddresses, CancellationToken cancellationToken) =>
         {
-            var transport = new SqsTransport(enableDelayedDelivery: false) { DoNotWrapOutgoingMessages = true };
+            var sqs = sp.GetRequiredService<IAmazonSQS>();
+            var sns = sp.GetRequiredService<IAmazonSimpleNotificationService>();
+#pragma warning disable NSBSQSEXP0001
+            var transport = new SqsTransport(sqs, sns, enableDelayedDelivery: false) { DoNotWrapOutgoingMessages = true };
+#pragma warning restore NSBSQSEXP0001
+
             transportConfig?.Invoke(transport);
 
-            var infrastructure =
-                await transport.Initialize(hostSettings, receivers, sendingAddresses, cancellationToken);
+            var infrastructure = await transport.Initialize(hostSettings, receivers, sendingAddresses, cancellationToken);
 
             var configuration = sp.GetRequiredService<Configuration>();
 
