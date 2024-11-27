354560005c1466eccf42c8a7e481219be9398db1
SzymonPobiega <szymon.pobiega@gmail.com>
2024-11-27 10:45:30 +0100

    Remove the submodule

--- update-submodules.sh
+++ update-submodules.sh
@@ -5,11 +5,6 @@ cd NServiceBus.AmazonSQS
 git fetch origin
 git reset --hard origin/tf527
 
-cd ../NServiceBus.RabbitMQ
-
-git fetch origin
-git reset --hard origin/tf527
-
 cd ../NServiceBus.Transport.AzureServiceBus
 
 git fetch origin
