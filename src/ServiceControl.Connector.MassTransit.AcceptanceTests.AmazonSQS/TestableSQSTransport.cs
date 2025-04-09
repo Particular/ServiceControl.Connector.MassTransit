74b493c75e1b9aa44c2709766d01b31d2b8be74e
Laila Bougria <laila@igindo.com>
2025-04-09 16:09:34 +0200

    remove unneeded pragma

--- src/ServiceControl.Connector.MassTransit.AcceptanceTests.AmazonSQS/TestableSQSTransport.cs
+++ src/ServiceControl.Connector.MassTransit.AcceptanceTests.AmazonSQS/TestableSQSTransport.cs
@@ -6,9 +6,7 @@ using NServiceBus.AcceptanceTests;
 public class TestableSQSTransport : SqsTransport
 {
     public TestableSQSTransport(string namePrefix)
-#pragma warning disable NSBSQSEXP0001
         : base(CreateSqsClient(), CreateSnsClient(), disableUnrestrictedDelayedDelivery: true)
-#pragma warning restore NSBSQSEXP0001
     {
         QueueNamePrefix = namePrefix;
         TopicNamePrefix = namePrefix;
